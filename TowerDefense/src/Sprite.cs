280a78ce7b75014025f2d538ba1443094c506f7a
B4NX <nikko.rush@gmail.com>
2013-05-08 18:11:06 -0700

    Initial Commit

---
+++ TowerDefense/src/Sprite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefense.src {
+    class Sprite {
+        Texture2D texture;
+        Vector2 pos,speed;
+        String asset;
+        public Sprite(Vector2 speed, Vector2 pos,String asset) {
+            this.speed=speed;
+            this.pos = pos;
+            this.asset = asset;
+        }
+
+        public virtual void LoadContent(ContentManager content){
+            this.texture = content.Load<Texture2D>(this.asset);
+        }
+
+        public virtual void Update() {
+            this.pos += this.speed;
+        }
+
+        public virtual void Draw(SpriteBatch spritebatch) {
+            spritebatch.Draw(this.texture, this.pos, Color.Transparent);
+        }
+    }
+}
