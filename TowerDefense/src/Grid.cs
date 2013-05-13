9de425447897250d194c1e0df02ac58496c1b8f5
Nikko Rush <nikko.rush@gmail.com>
2013-05-13 10:27:56 -0700

    5-13-2013b
    More work on the grid, adds a list of the points in each row.
    Switched from tuples to Vector2 to represents each point.

--- TowerDefense/src/Grid.cs
+++ TowerDefense/src/Grid.cs
@@ -11,20 +11,21 @@ namespace TowerDefense.src {
     class Grid {
         int width, height, rows, columns;
         List<Vector2> PointList;
-        List<List<Tuple<Vector2,Vector2>>> GridList;
+        List<List<Vector2>> RowList;//Holds to the list of the points in each row, each row gets its own list
 
         public Grid(GraphicsDevice graphics) {
-            this.height = 10;
-            this.width = 10;
+            this.height = 160;
+            this.width = 160;
 
-            this.rows = graphics.Viewport.Width / this.width+1;
-            this.columns = graphics.Viewport.Height / this.height+1;
+            this.columns = graphics.Viewport.Width / this.width+1;
+            this.rows = graphics.Viewport.Height / this.height+1;
 
             this.PointList = new List<Vector2>();
-            this.GridList = new List<List<Tuple<Vector2,Vector2>>>();
+            this.RowList = new List<List<Vector2>>();
 
             this.CreatePointList();
-            Console.WriteLine(PointList[3]);
+            this.CreateGridList();
+            Console.Write("");
         }
 
         private void CreatePointList() {
@@ -42,11 +43,21 @@ namespace TowerDefense.src {
         }
 
         private void CreateGridList(){
+            float xVal = 0;
+            float yVal = 0;
+            List<Vector2> Holder;
 
             for (int i = 0; i <= this.rows; i++) {
+                Holder = new List<Vector2>();
+
                 for (int j = 0; j <= this.columns; j++) {
-                    
+                    Holder.Add(new Vector2(xVal, yVal));
+                    xVal += this.width;
                 }
+
+                yVal += this.height;
+                xVal = 0;
+                this.RowList.Add(Holder);
             }
         }
     }
